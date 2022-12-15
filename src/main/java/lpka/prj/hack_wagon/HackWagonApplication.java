02df00612b96e72166b940264cfc02f8a8232432
StromenkoNA <n.stromenko@id-mt.ru>
2022-12-15 23:48:31 +0800

    Init

---
+++ src/main/java/lpka/prj/hack_wagon/HackWagonApplication.java
@@ -0,0 +1,13 @@
+package lpka.prj.hack_wagon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackWagonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackWagonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lpka/prj/hack_wagon/HackWagonApplicationTests.java
@@ -0,0 +1,13 @@
+package lpka.prj.hack_wagon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackWagonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
